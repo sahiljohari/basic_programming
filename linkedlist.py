6eb5a5a497645af7bb2188b806b52aad282675a9
sahiljohari <darewithsj@gmail.com>
2018-08-13 20:14:56 -0500

    First commit

---
+++ linkedlist.py
@@ -0,0 +1,86 @@
+# Program to implement Linked List
+
+# Create a node class
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    # Traverse a linked list
+    def traverse(self):
+        node = self # start from the head node
+        while node is not None:
+            print(node.val)
+            node = node.next
+
+    # Add a node to the linked list
+    def add_node(self, obj):
+        node = self
+        while node.next is not None:
+            node = node.next
+        node.next = obj
+
+    # Delete a node from a linked list
+    def delete_node(self, val):
+        node = self
+        while node.next is not None:
+            if node.next.val == val:
+                node.next = node.next.next
+            node = node.next
+
+    # Remove duplicates from a linked list
+    def remove_dups(self):
+        buffer = []
+        node = self
+        while node is not None:
+            if node.val not in buffer:
+                buffer.append(node.val)
+            else:
+                self.delete_node(node.val)
+            node = node.next
+
+    # Get the kth to last element from a linked list
+    def sublist(self, k):
+        node = self
+        count = 0
+        while node is not None:
+            if count == k:
+                node.traverse()
+                break
+            else:
+                count += 1
+            node = node.next
+
+if __name__=="__main__":
+    head = Node(1)
+    node_1 = Node(2)
+    node_2 = Node(3)
+    node_3 = Node(4)
+    head.add_node(node_1)
+    head.add_node(node_2)
+    head.add_node(node_3)
+
+    print("Adding nodes...")
+    head.traverse()
+
+    print("Deleting nodes...")
+    head.delete_node(3)
+    head.traverse()
+
+    print("Adding a duplicate node...")
+    node_4 = Node(4)
+    head.add_node(node_4)
+    head.traverse()
+
+    print("After removing duplicate element...")
+    head.remove_dups()
+    head.traverse()
+
+    print("Adding more nodes...")
+    head.add_node(Node(5))
+    head.add_node(Node(6))
+    head.add_node(Node(7))
+    head.traverse()
+
+    print("Printing list from 3rd to last...")
+    head.sublist(3)
