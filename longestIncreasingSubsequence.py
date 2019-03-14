7d6fddeab1ed7c3ac481f188bd9636ee6053b579
sahiljohari <darewithsj@gmail.com>
2019-03-13 19:39:12 -0500

    O(n^2) time

---
+++ longestIncreasingSubsequence.py
@@ -0,0 +1,27 @@
+# Given an array of numbers, find the length of the longest increasing subsequence in the array. 
+# The subsequence does not necessarily have to be contiguous.
+
+# For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], 
+# the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
+
+
+# Using Dynamic Programming
+def longestIncreasingSubsequence(arr):
+    if not arr:
+        return 0
+    
+    cache = [1] * len(arr)
+
+    for i in range(1, len(arr)):
+        for j in range(i):
+            if arr[i] > arr[j]:
+                cache[i] = max(cache[i], cache[j]+1)
+    print(cache)
+
+    return max(cache)
+
+
+if __name__ == "__main__":
+    sequence = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+
+    print("The longest increasing subsequence has length %d" % longestIncreasingSubsequence(sequence))
