3cb83a747a68d1b62c4b12b51320edf52c2b44d8
sahiljohari <darewithsj@gmail.com>
2019-03-27 20:03:23 -0500

    Added node deletion

--- Trees/binary_tree.py
+++ Trees/binary_tree.py
@@ -6,7 +6,7 @@ class Node:
         self.left = None
         self.right = None
 
-# Inorder traversal of binary tree
+# Inorder traversal of binary tree (or Breadth-first Search)
 def inorder(root):
     if root is not None:
         inorder(root.left)
@@ -35,3 +35,79 @@ def insertNode(root, val):
         else:
             q.append(temp.right)
 
+# Delete the deepest node in the tree
+def deleteDeepest(root, nodeToDelete):
+    q = []
+    q.append(root)
+
+    while(len(q)):
+        temp = q[0]
+        q.pop(0)
+
+        if temp.right:
+            if temp.right == nodeToDelete:
+                temp.right = None
+                del(nodeToDelete)
+                return
+            else:
+                q.append(temp.right)
+
+        if temp.left:
+            if temp.left == nodeToDelete:
+                temp.left = None
+                del(nodeToDelete)
+                return
+            else:
+                q.append(temp.left)
+
+# Delete a given node
+def deleteNode(root, val):
+    q = []
+    q.append(root)
+
+    key_node = Node(None)
+
+    while(len(q)):
+        temp = q[0]
+        q.pop(0)
+
+        if temp.val == val:
+            key_node = temp
+
+        if temp.left:
+            q.append(temp.left)
+
+        if temp.right:
+            q.append(temp.right)
+
+    x = temp.val
+    deleteDeepest(root, temp)
+    key_node.val = x
+
+
+def main():
+    root = Node(13)
+    # Create left sub-tree
+    root.left = Node(12)
+    root.left.left = Node(4)
+    root.left.right = Node(19)
+    # Create right sub-tree
+    root.right = Node(10)
+    root.right.left = Node(16)
+
+    print("Here is the initial tree (Inorder traversal):")
+    inorder(root)
+
+    insert_key = int(input("\nEnter the node value to be inserted: "))
+    insertNode(root, insert_key)
+    print("Inorder traversal after insertion:")
+    inorder(root)
+
+    delete_key = int(input("\nEnter the node value to be deleted: "))
+    deleteNode(root, delete_key)
+    # After deletion
+    print("Inorder traversal after deletion:")
+    inorder(root)
+
+if __name__ == "__main__":
+    main()
