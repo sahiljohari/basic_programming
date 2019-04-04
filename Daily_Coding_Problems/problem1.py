36fa1d355881ab34b52e294aba4a922bd17d68cb
sahiljohari <darewithsj@gmail.com>
2019-04-03 19:15:22 -0500

    Optimized to linear time

--- Daily_Coding_Problems/problem1.py
+++ Daily_Coding_Problems/problem1.py
@@ -1,22 +1,16 @@
 # Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
-
-def two_sum_1(arr, k):
-    for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
-            if abs(k-arr[i]) == arr[j]:
-                return True
-            
-    return False
-
-def two_sum_2(arr, k):
-    for i in arr:
-            if abs(k-i) in arr:
-                return True
+def two_sum(num_list, k):
+    num_seen = set()
+        
+    for first_num in num_list:
+        second_num = k - first_num
+        if second_num in num_seen:
+            return True
+        num_seen.add(first_num)
     return False
 
 if __name__ == "__main__":
-    arr = [-1, 19]
+    arr = [1,2,3,5,10,13,15]
     k = 18
     
-    print("Output using first approach:",two_sum_1(arr, k))
-    print("Output using second approach:",two_sum_2(arr, k))
+    print("Output using first approach:",two_sum(arr, k))
