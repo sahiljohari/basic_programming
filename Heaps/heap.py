f1296d8b3b57a466f7481164cab18a62a2788deb
Sahil Johari <darewithsj@gmail.com>
2021-04-13 17:02:19 -0400

    Added insert and remove methods - Added heapify operations

--- Heaps/heap.py
+++ Heaps/heap.py
@@ -77,3 +77,84 @@ class MinHeap:
             self.storage[index2],
             self.storage[index1],
         )
+
+    def insert(self, data):
+        """
+        Inserts data into the heap and performs a heapify operation
+        """
+        if self.isFull():
+            raise ("Heap is full")
+        self.storage[self.size] = data
+        self.size += 1
+        # self.heapifyUp()
+        self.heapifyUpRec(self.size - 1)
+
+    def heapifyUp(self):
+        """
+        Performs a heapify operation in upward direction
+        """
+        index = self.size - 1
+
+        while self.hasParent(index) and self.parent(index) > self.storage[index]:
+            self.swap(self.getParentIndex(index), index)
+            index = self.getParentIndex(index)
+
+    def heapifyUpRec(self, index):
+        """
+        Performs heapifyUp() operation recursively
+        """
+        if self.hasParent(index) and self.parent(index) > self.storage[index]:
+            self.swap(self.getParentIndex(index), index)
+            self.heapifyUpRec(self.getParentIndex(index))
+
+    def removeMin(self):
+        """
+        Removes topmost element from the heap and performs a heapify operation
+        """
+        if self.size == 0:
+            raise ("Heap is empty")
+
+        data = self.storage[0]
+        self.storage[0] = self.storage[self.size - 1]
+        self.size -= 1
+        # self.heapifyDown()
+        self.heapifyDownRec(0)
+        return data
+
+    def heapifyDown(self):
+        """
+        Performs a heapify operation in downward direction
+        """
+        index = 0
+
+        while self.hasLeftChild(index):
+            smallerChildIndex = self.getLeftChildIndex(index)
+            if self.hasRightChild(index) and self.rightChild(index) < self.leftChild(
+                index
+            ):
+                smallerChildIndex = self.getRightChildIndex(index)
+
+            if self.storage[index] < self.storage[smallerChildIndex]:
+                break
+            else:
+                self.swap(index, smallerChildIndex)
+
+            index = smallerChildIndex
+
+    def heapifyDownRec(self, index):
+        """
+        Performs heapifyDown() operation recursively
+        """
+        smallest = index
+
+        if self.hasLeftChild(index) and self.storage[smallest] > self.leftChild(index):
+            smallest = self.getLeftChildIndex(index)
+
+        if self.hasRightChild(index) and self.storage[smallest] > self.rightChild(
+            index
+        ):
+            smallest = self.getRightChildIndex(index)
+
+        if smallest != index:
+            self.swap(index, smallest)
+            self.heapifyDownRec(smallest)
